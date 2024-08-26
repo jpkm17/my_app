c9cc78a2d9ec2f63bf465aea13df112b9197806a
João P. Meneguesso <jpm83633@gmail.com>
2024-08-26 08:35:29 -0300

    aprendendo a usar o grid

--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,9 +1,21 @@
 import { useState, useEffect } from "react"
+import { makeStyles } from "@material-ui/core"
 import axios from "axios"
+import Grid from '@material-ui/core/Grid'
 
 import CustomerCard from "../components/CustomerCard"
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    card: {
+        margin: theme.spacing(2)
+    }
+}));
+
 const Customers = () => {
+    const classes = useStyles()
     const [customers, setCustomers] = useState([])
 
     console.log(customers)
@@ -20,17 +32,21 @@ const Customers = () => {
     return (
         <>
             <h1>Customers</h1>
-            {
-            customers.map(item => (
-                <CustomerCard
-                    name={item.first_name}
-                    lastName={item.last_name}
-                    email={item.email}
-                    avatar={item.avatar}
-                />
-
-            ))
-            }
+            <Grid container>
+             {
+                customers.map(item => (
+                    <Grid item xs={12} md={4}>
+                        <CustomerCard
+                            name={item.first_name}
+                            lastName={item.last_name}
+                            email={item.email}
+                            avatar={item.avatar}
+                            className={classes.card}
+                        />
+                    </Grid>
+                ))
+             }
+            </Grid>
         </>
     );
 }
